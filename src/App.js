fafc1c1de297184c1a33b691fce76e5a92afe804
Mami Nishiwaki <mamypoco@gmail.com>
2022-09-27 14:52:34 -0700

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import CampsiteDirectoryPage from "./pages/CampsitesDirectoryPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
    return (
       <div className="App">
          <Header />
-         <CampsiteDirectoryPage />
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="contact" element={<ContactPage />} />
+            <Route path="directory" element={<CampsiteDirectoryPage />} />
+         </Routes>
          <Footer />
       </div>
    );
